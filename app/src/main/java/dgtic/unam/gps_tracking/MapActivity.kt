1a4a872de45bec6de4040f226a7f10b9b3ef6165
MoY8462 <moy.sav21@hotmail.com>
2023-01-30 23:25:46 -0600

    feat: :sparkles: avtivities login sigup and map add

--- app/src/main/java/dgtic/unam/gps_tracking/MapActivity.kt
+++ app/src/main/java/dgtic/unam/gps_tracking/MapActivity.kt
@@ -3,9 +3,9 @@ package dgtic.unam.gps_tracking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_map)
     }
 }
