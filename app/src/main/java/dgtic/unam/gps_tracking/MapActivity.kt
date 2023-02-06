b0ee5a248aa6229873db03491671042585b88d91
MoY8462 <moy.sav21@hotmail.com>
2023-02-06 10:22:10 -0600

    feat: :sparkles: show marker in map

--- app/src/main/java/dgtic/unam/gps_tracking/MapActivity.kt
+++ app/src/main/java/dgtic/unam/gps_tracking/MapActivity.kt
@@ -4,42 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import dgtic.unam.gps_tracking.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    /*
     //ViewBinding
     private lateinit var binding: ActivityMapBinding
-
     //ActionBar
     private lateinit var actionBar: ActionBar
-
     //FirebaseAuth
     private lateinit var firebaseAuth: FirebaseAuth
+    */
+    private lateinit var map : GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMapBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_map)
+        createFragment()
 
-        /*
-        //Configure actionbar
+
+        /*/Configure actionbar
         actionBar = supportActionBar!!
         actionBar.title = "Map"
-
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
-
         //HandleLoggout
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
             checkUser()
         }*/
     }
-    /*
+
+    private fun createFragment() {
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
     private fun checkUser() {
-        //Check user is logges in or not
+        /*/Check user is logges in or not
         val firebaseUser = firebaseAuth.currentUser
         if(firebaseUser != null){
             //User is logged in
@@ -50,6 +64,23 @@ class MapActivity : AppCompatActivity() {
             //User is not loggedin
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
-        }
-    }*/
+        }*/
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng ( 19.3329456, -99.1851108)
+        val marker = MarkerOptions().position(coordinates).title("CU")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
+    }
 }
+
